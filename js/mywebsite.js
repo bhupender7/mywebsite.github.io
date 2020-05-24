4b25861c3bfe1f7d5371c1b7a87df68d908f0e95
bhupender7 <imvikashsingh8596@gmail.com>
2020-05-24 19:20:05 +0530

    mywebsite
    this is my website........
    just enjoy.........

---
+++ js/mywebsite.js
@@ -0,0 +1,77 @@
+
+$(document).ready(function () {
+
+ $('.js--wp-1').waypoint(function(direction) {
+    $('.js--wp-1').addClass('animated fadeInLeft');
+    }, {
+        offset: '40%'
+    });
+
+   $('.js--wp-2').waypoint(function(direction) {
+    $('.js--wp-2').addClass('animated fadeInRight');
+    }, {
+        offset: '40%'
+    });
+
+      $('.js--wp-3').waypoint(function(direction) {
+    $('.js--wp-3').addClass('animated rotateInUpRight');
+    }, {
+        offset: '40%'
+    });
+
+         $('.js--wp-4').waypoint(function(direction) {
+    $('.js--wp-4').addClass('animated rotateInUpLeft');
+    }, {
+        offset: '40%'
+    });
+
+     $('.js--wp-5').waypoint(function(direction) {
+    $('.js--wp-5').addClass('animated fadeIn');
+    }, {
+        offset: '50%'
+    });
+
+     $('.js--wp-6').waypoint(function(direction) {
+    $('.js--wp-6').addClass('animated fadeInRight');
+    }, {
+        offset: '50%'
+    });
+
+     $('.js--wp-7').waypoint(function(direction) {
+    $('.js--wp-7').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+      $('.js--wp-8').waypoint(function(direction) {
+    $('.js--wp-8').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+       $('.js--wp-9').waypoint(function(direction) {
+    $('.js--wp-9').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+        $('.js--wp-10').waypoint(function(direction) {
+    $('.js--wp-10').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+         $('.js--wp-11').waypoint(function(direction) {
+    $('.js--wp-11').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+          $('.js--wp-12').waypoint(function(direction) {
+    $('.js--wp-12').addClass('animated fadeInUp');
+    }, {
+        offset: '50%'
+    });
+
+
+});
